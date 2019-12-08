efaa0cb53f36a68e7dd53299c9b1d0271eff8f5a
Traci Crawford <tracicrawford@tracis-air.attlocal.net>
2019-12-07 20:47:23 -0600

    completed react-1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import './index.css';
+import TopicBrowser from './components/TopicBrowser/TopicBrowser'
 
 class App extends Component {
   render() {
-    
+    return (
+      <TopicBrowser />
+    )
   }
 }
 
 export default App;
+
+
+
+
